6e77f2a13bf5766de47ac08b580ea782c830c6b8
kewool <crobrine@naver.com>
2021-12-19 18:03:29 +0900

    2869

--- BOJ/2869 달팽이는 올라가고 싶다.c
+++ BOJ/2869 달팽이는 올라가고 싶다.c
@@ -20,9 +20,6 @@
 int main() {
 	int a, b, v;
 	scanf("%d %d %d", &a, &b, &v);
-	if ((v - b) % (a - b))
-		printf("%d", (v - b) / (a - b) + 1);
-	else
-		printf("%d", (v - b) / (a - b));
+	(v - b) % (a - b) ? printf("%d", (v - b) / (a - b) + 1) : printf("%d", (v - b) / (a - b));
 	return 0;
 }
