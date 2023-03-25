633f9cf4f6a354a78ba03ce214de3a5e92d17e6c
kewool <kkttyy324@gmail.com>
2023-03-26 04:32:57 +0900

    1940

---
+++ BOJ/1940 주몽.cpp
@@ -0,0 +1,54 @@
+/*
+문제
+주몽은 철기군을 양성하기 위한 프로젝트에 나섰다. 그래서 야철대장을 통해 철기군이 입을 갑옷을 만들게 하였다. 야철대장은 주몽의 명에 따르기 위하여 연구에 착수하던 중 아래와 같은 사실을 발견하게 되었다.
+
+갑옷을 만드는 재료들은 각각 고유한 번호를 가지고 있다. 갑옷은 두 개의 재료로 만드는데 두 재료의 고유한 번호를 합쳐서 M(1 ≤ M ≤ 10,000,000)이 되면 갑옷이 만들어 지게 된다. 야철대장은 자신이 만들고 있는 재료를 가지고 갑옷을 몇 개나 만들 수 있는지 궁금해졌다. 이러한 궁금증을 풀어 주기 위하여 N(1 ≤ N ≤ 15,000) 개의 재료와 M이 주어졌을 때 몇 개의 갑옷을 만들 수 있는지를 구하는 프로그램을 작성하시오.
+
+입력
+첫째 줄에는 재료의 개수 N(1 ≤ N ≤ 15,000)이 주어진다. 그리고 두 번째 줄에는 갑옷을 만드는데 필요한 수 M(1 ≤ M ≤ 10,000,000) 주어진다. 그리고 마지막으로 셋째 줄에는 N개의 재료들이 가진 고유한 번호들이 공백을 사이에 두고 주어진다. 고유한 번호는 100,000보다 작거나 같은 자연수이다.
+
+출력
+첫째 줄에 갑옷을 만들 수 있는 개수를 출력한다.
+
+풀이
+arr의 처음과 끝을 가리키는 left와 right를 이용해서 left + right가 M보다 크면 right--, M보다 작으면 left++,
+같으면 result++, left++, right--를 left와 right가 같거나 left가 right보다 커질 때까지 반복한다.
+*/
+#include <bits/stdc++.h>
+#define loop(i,n) for(int i = 0; i < n; i++)
+typedef long long ll;
+using namespace std;
+
+int N, M, left_i, right_i, tmp, res;
+vector<int> arr;
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    cin >> N >> M;
+
+    loop(i, N) {
+        cin >> tmp;
+        arr.push_back(tmp);
+    }
+    sort(arr.begin(), arr.end());
+
+    right_i = arr.size() - 1;
+
+    while (left_i != right_i && left_i < right_i) {
+        tmp = arr[left_i] + arr[right_i];
+        if (tmp < M) left_i++;
+        else if (tmp > M) right_i--;
+        else {
+            res++;
+            left_i++;
+            right_i--;
+        }
+    }
+
+    cout << res;
+
+    return 0;
+}
